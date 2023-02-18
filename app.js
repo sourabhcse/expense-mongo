7f6fe4712a4d7e7a3966bd24b6d7a6759d5aa441
sourabhcse <sourabhsaini201098>
2023-02-18 20:23:44 +0530

    app

--- app.js
+++ app.js
@@ -5,37 +5,36 @@ const dotenv = require('dotenv');
 dotenv.config();
 const bodyParser = require('body-parser');
 var cors=require('cors')
-const sequelize = require('./util/database');
-
+// const sequelize = require('./util/database');
+const mongoose = require('mongoose')
 const app = express();
 // app.use(helmet())
 app.use(cors())
 app.use(bodyParser.json());
-
 const User = require('./models/users');
 const Expense=require('./models/expenses');
 const Order=require('./models/orders');
 const Forgotpassword=require('./models/forgotpassword')
 const Filelink=require('./models/filelink')
 
-User.hasMany(Expense)
-Expense.belongsTo(User)
+// User.hasMany(Expense)
+// Expense.belongsTo(User)
 
-User.hasMany(Order)
-Order.belongsTo(User)
+// User.hasMany(Order)
+// Order.belongsTo(User)
 
 
-User.hasMany(Forgotpassword);
-Forgotpassword.belongsTo(User);
+// User.hasMany(Forgotpassword);
+// Forgotpassword.belongsTo(User);
 
-User.hasMany(Filelink);
-Filelink.belongsTo(User)
+// User.hasMany(Filelink);
+// Filelink.belongsTo(User)
 
 
 const UserRoutes=require('./routes/user')
 const ExpenseRoutes=require('./routes/expense')
 const PremiumRoutes=require('./routes/premium')
-const ForgotpasswordRoutes=require('./routes/forgotpassword')
+// const ForgotpasswordRoutes=require('./routes/forgotpassword')
 const RazorpayRoutes=require('./routes/razorpay')
 
 
@@ -43,21 +42,19 @@ const RazorpayRoutes=require('./routes/razorpay')
 app.use(UserRoutes)
 app.use(ExpenseRoutes)
 app.use(PremiumRoutes)
-app.use(ForgotpasswordRoutes)
+// app.use(ForgotpasswordRoutes)
 app.use(RazorpayRoutes)
 
 
-app.use((req,res)=>{
-  console.log('urlll',req.url)
-  res.sendFile(path.join(__dirname,`public/views/${req.url}`));
+// app.use((req,res)=>{
+//   console.log('urlll',req.url)
+//   res.sendFile(path.join(__dirname,`public/views/${req.url}`));
   
+// })
+
+mongoose.connect('mongodb+srv://sourabhsaini201098:2r5GFHXDzw5JPkX9@cluster0.ypqknjp.mongodb.net/?retryWrites=true&w=majority')
+.then(result=>{
+  app.listen(4000)
+}).catch(err=>{
+  console.log(err)
 })
-sequelize
-.sync()
-.then(result => {
-   console.log(result)
-   app.listen(4000)
-  })
-  .catch((err)=>{
-    console.log(err)
-  })
