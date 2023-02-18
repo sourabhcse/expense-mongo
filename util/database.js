7b4f9c0a84b004d0d333570a22887b520c883317
sourabhcse <sourabhsaini201098>
2023-02-18 20:19:41 +0530

    db

--- util/database.js
+++ util/database.js
@@ -1,8 +1,23 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-  dialect: 'mysql',
-  host: process.env.DB_HOST
-});
-
-module.exports = sequelize;
+const mongodb = require('mongodb');
+const MongoCilent = mongodb.MongoClient;
+let _db;
+const mongoConnect = callback =>{
+  MongoCilent.connect('mongodb+srv://sourabhsaini201098:2r5GFHXDzw5JPkX9@cluster0.ypqknjp.mongodb.net/?retryWrites=true&w=majority')
+  .then(client =>{
+    console.log('connected')
+    // _db=client.db();
+    callback(client)
+  })
+  .catch(err=>{
+    console.log(err)
+    throw err;
+  })
+}
+const getDb=()=>{
+  if(_db){
+    return _db;
+  }
+  throw "no database found!"
+}
+exports.mongoConnect=mongoConnect;
+exports.getDb=getDb;
