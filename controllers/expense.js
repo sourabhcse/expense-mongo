ff784fd11225ab8ff7ffa901e9205a997616462d
sourabhcse <sourabhsaini201098>
2023-02-18 20:30:44 +0530

    update

--- controllers/expense.js
+++ controllers/expense.js
@@ -9,17 +9,12 @@ function isstringinvalid(string){
 exports.postExpense=async(req,res,next)=>{
     try{
     const{expenseamount,description,category}=req.body;
-   
-    console.log(expenseamount,description,category)
-
     if(isstringinvalid(expenseamount) || isstringinvalid(description) || isstringinvalid(category)){
         return res.status(400).json({success:false,message:'parameter is missing'})
     }
-    // req.user.createExpense({expenseamount,description,category})
-    await Expense.create({expenseamount,description,category,userId:req.user.id})
-    .then(expense=>{
-        return res.status(201).json({expense,success:true})
-    })
+    const expense =new Expense({expenseamount,description,category,userId:req.user})
+    expense.save();
+    res.status(201).json({expense,success:true})
 }
     catch{err=>{
         return res.status(500).json({success:false,error:err})
@@ -28,48 +23,25 @@ exports.postExpense=async(req,res,next)=>{
 
 }
 exports.getExpense=async(req,res,next)=>{
-    // req.user.getExpenses().then(result=>{
-    //     console.log(result)
-    // })
-    const page = +req.query.page || 1;
-    const NUMBER_OF_EXPENSES_PER_PAGE=3;
-    let total_items
-    Expense.count({where:{userId:req.user.id}})
-    .then((total)=>{
-        total_items=total
-        return Expense.findAll({where:{userId:req.user.id},offset:(page-1)*NUMBER_OF_EXPENSES_PER_PAGE,
-        limit:NUMBER_OF_EXPENSES_PER_PAGE})
-    })
-   
+    try{
+  const expenses= await Expense.find({userId:req.user._id})
 
-    .then(expenses=>{
-          const pagination={
-            currentPage:page,
-            hasNextPage:NUMBER_OF_EXPENSES_PER_PAGE *page<total_items,
-            nextPage:page + 1,
-            hasPreviousPage:page>1,
-            previousPage:page-1,
-            lastPage:Math.ceil(total_items/NUMBER_OF_EXPENSES_PER_PAGE),
-
-        }
-        res.status(200).json({expenses,pagination,success:true})
-    })
-    .catch(err=>{
-        res.status(500).json({error:err,success:false})
-    })
+    res.status(200).json({expenses,success:true})
+    }catch(err){
+        res.status(500).json({error:err,success:false}) 
+    }
 }
 exports.deleteExpense=async(req,res,next)=>{
+    try{
     const expenseid=req.params.expenseid;
+    console.log(expenseid)
     if(isstringinvalid(expenseid)){
        return res.status(400).json({success:false,message:'bad parameter'})
     }
-    Expense.destroy({where:{id:expenseid,userId:req.user.id}}).then((noofrows)=>{
-        if(noofrows===0){
-            return res.status(400).json({success:false,message:'user doesnot belong to their expenses'})
-        }
-        return res.status(200).json({success:true,message:'Deleted successfully'})
-    })
-    .catch(err=>{
-        return res.status(500).json({success:false,message:"failed"})
-    })
+    const expense=await Expense.deleteOne({_id:expenseid,userId:req.user})
+    res.status(200).json({success:true,message:'Deleted Successfully'})
+}
+catch(err){
+    res.status(500).json({message:'internal Error',success:false})
+}
 }
